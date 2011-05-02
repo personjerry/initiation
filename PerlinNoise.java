9a6e035c1dcb85d867ea0c9f29c395a3adca42ed
CharlesZinn <charles.zinn@gmail.com>
2011-05-02 18:45:39 -0400

    PerlinNoise.java now generates smooth terrain at any zoom level.

--- PerlinNoise.java
+++ PerlinNoise.java
@@ -10,11 +10,12 @@ public class PerlinNoise {
   static final int SEED_NOISE_GEN = 13397; //random prime (orig. 1013)
   
   public static void main(String[] args) {
-    PerlinNoise p=new PerlinNoise(20,.6,984);
-    double zoom=.125; //lower, the more zoomed in; .5 seems good
+    PerlinNoise p=new PerlinNoise(10,.4,"molly".hashCode());
+    double zoom=0.25; //lower, the more zoomed in; .5 seems good
     for(double i=0;i<20*zoom;i+=zoom) {
       for(double j=0;j<50*zoom;j+=zoom) {
-        if(p.perlin_noise(i,j)<0) {
+        double pn=p.perlin_noise(i,j);
+        if(pn>0) {
           System.out.print("#");
         } else {
           System.out.print(".");
@@ -37,17 +38,20 @@ public class PerlinNoise {
   
       return 1.0 - ( (n * (n * n * 60493 + 19990303) + 1376312589) & 0x7fffffff ) / 1073741824.0;
    }
-  double interpolate(double a,double b,double x) {
-    double ft=x*Math.PI;
+  static double interpolate(double a,double b,double x) {
+    /*double ft=x*Math.PI;
     double f=(Math.cos(ft)+1)*.5;
-    return f*(a+b)+b;
+    return f*(a+b)+b;*/
+    double ft = x * Math.PI;
+    double f = (1 - Math.cos(ft)) * .5;
+    return  a*(1-f) + b*f;
   }
   double interpolated_noise(double x,double y,int octave) {
-    int int_x=(int)x;
-    double frac_x=x-int_x;
+    int int_x=(int)Math.floor(x);
+    double frac_x=x-(int_x+0.0);
     
-    int int_y=(int)y;
-    double frac_y=y-int_y;
+    int int_y=(int)Math.floor(y);
+    double frac_y=y-(int_y+0.0);
     
     double v1=noise(int_x,int_y,octave);
     double v2=noise(int_x+1,int_y,octave);
